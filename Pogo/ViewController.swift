597cef730b1a20aaff70b08b1f90ad49b6f94815
Nebula <github@itsnebula.net>
2022-11-13 09:28:20 -0800

    use nightly and make a symbolic link to /var/jb

--- Pogo/ViewController.swift
+++ Pogo/ViewController.swift
@@ -83,7 +83,7 @@ class ViewController: BaseViewController {
             return
         }
          
-        guard let deb = Bundle.main.path(forResource: "org.coolstar.sileo_2.3_iphoneos-arm", ofType: "deb") else {
+        guard let deb = Bundle.main.path(forResource: "org.coolstar.sileonightly_2.420221112.1668273314.01ea368_iphoneos-arm", ofType: "deb") else {
             NSLog("[POGO] Could not find deb")
             return
         }
@@ -150,7 +150,17 @@ class ViewController: BaseViewController {
                                             self.statusLabel?.text = "failed to uicache \(ret)"
                                             return
                                         }
-                                        self.statusLabel?.text = "uicache succesful, have fun!"
+                                        self.statusLabel?.text = "Make symbolic link"
+                                        DispatchQueue.global(qos: .utility).async {
+                                            let ret = spawn(command: "/bin/ln", args: ["-s", "/", "/var/jb"], root: true)
+                                            DispatchQueue.main.async {
+                                                if ret != 0 {
+                                                    self.statusLabel?.text = "failed to ln \(ret)"
+                                                    return
+                                                }
+                                                self.statusLabel?.text = "link succesful, have fun!"
+                                            }
+                                        }
                                     }
                                 }
                             }
